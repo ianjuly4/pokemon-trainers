9d06b7e5c984080ff54e538cdce583e8599ff3e9
Ian July <ianjuly4@gmail.com>
2024-06-06 02:37:13 -0600

    trainer cli and helper (list_trainers) update)

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,7 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    list_trainers
 )
 
 
@@ -10,18 +10,44 @@ def main():
     while True:
         menu()
         choice = input("> ")
-        if choice == "0":
+
+        if choice == "1":
+            list_trainers()
+            while True:
+                trainers_menu()
+                trainers_choice = (">"):
+                if trainers_choice == "A":
+                    selected_trainer()
+                    list_trainers()
+                elif trainers_choice == "B":
+                    input_trainer
+        elif choice == "0":
             exit_program()
-        elif choice == "1":
-            helper_1()
         else:
-            print("Invalid choice")
+            print("Invalid choice, please select a choice from the given menu.")
+
+
+
 
 
 def menu():
     print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. List of Pokemon Trainers")
+    print("0. Exit Pokemon Trainers")
+
+def trainers_menu():
+    print("                                                     ")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("                                                     ")
+    print(" Please select an option from the menu:")
+    print("     A. Select a pokemon trainer to view their pokemon.")
+    print("     B. Input a new pokemon trainer.")
+    print("     C. Delete a pokemon trainer.")
+    print("     D. Exit to main menu.")
+    print("                                                     ")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("                                                     ")
+
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -4,6 +4,39 @@ from models.trainer import Trainer
 from models.__init__ import CURSOR, CONN
 from models.pokemon import Pokemon
 
+def list_trainers():
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("                             POKEMON TRAINERS                            ")
+    print("                                                                         ")
+
+    Trainer.create_table()
+    trainers = Trainer.get_all()
+    for trainer in trainers:
+        print(trainer.name)
+
+def input_new_trainer():
+    trainer_name = input("Please input the name of new pokemon trainer: ")
+    existing_trainer = Trainer.find_by_name(trainer_name)
+    if existing_trainer:
+        print("Trainer name already exists. Please enter a non-listed name.")
+    else:
+        try:
+            new_trainer = Trainer.create(trainer_name)
+            print(f'{new_trainer.trainer_name} created.')
+        except Exception as exc:
+            print("Error creating pokemon trainer:", exc)
+
+def selected_category():
+    category_select = input("Name of category: ")
+    category_expense = Expense.find_by_category(category_select)
+    if category_expense:
+       for expense in category_expense:
+           print(expense.expense_name, expense.expense_amount, expense.expense_month)
+    elif category_expense != Expense.find_by_category(category_select):
+        print("Selected category must reference a category in the categories database.")
+    else:
+        print("Selected category does not represent any current expense.")
+
 
 def exit_program():
     print("Goodbye!")
